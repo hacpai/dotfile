b39f2aab90289a50b7ff8257b030f77f1e34074f
henry-zhuang <mountainsea@outlook.com>
2015-05-07 12:16:09 +0800

    :beer: Configre Plugin thefuck

--- config.fish
+++ config.fish
@@ -14,3 +14,16 @@ set fish_plugins theme brew django emoji-clock fish-spec python z vundle ssh
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+# For thefuck
+function __thefuck_repl -d 'Replace operators into fish-compatible'
+    set -l tmp (echo $argv | sed 's/ && / ; and /g')
+    echo $tmp | sed 's/ || / ; or /g'
+end
+
+function fuck -d 'Correct your previous console command'
+    set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
+    thefuck $history[1] > $eval_script
+    eval (__thefuck_repl (cat $eval_script))
+    rm $eval_script
+end
