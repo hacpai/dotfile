765e26f93d55263c0cdee4505bcd70258ee91551
hacpai <balckwing@gmail.com>
2015-09-14 17:31:30 +0800

    :memo: Update config.fish

--- config.fish
+++ config.fish
@@ -1,36 +1,19 @@
 # Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+set -g OMF_PATH $HOME/.local/share/omf
+
+# Path to your oh-my-fish configuration.
+set -g OMF_CONFIG $HOME/.config/omf
+
+# Path to your z configuration.
+set -g Z_SCRIPT_PATH /usr/local/Cellar/z/1.8/etc/profile.d/z.sh
+
+# Only display the folder name
+set -g theme_short_path yes
+
+### Configuration required to load oh-my-fish ###
+# Note: Only add configurations that are required to be set before oh-my-fish is loaded.
+# For common configurations, we advise you to add them to your $OMF_CONFIG/init.fish file or
+# to create a custom plugin instead.
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-# Theme
-Theme 'robbyrussell'
-
-# All built-in plugins can be found at ~/.oh-my-fish/plugins/
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Enable plugins by adding their name separated by a space to the line below.
-Plugin "theme" 
-Plugin "brew" 
-Plugin "django"
-Plugin "emoji-clock" 
-Plugin "fish-spec" 
-Plugin "python" 
-Plugin "z" 
-Plugin "vundle"
-Plugin "ssh"
-
-# Path to your custom folder (default path is ~/.oh-my-fish/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-# For thefuck
-function __thefuck_repl -d 'Replace operators into fish-compatible'
-    set -l tmp (echo $argv | sed 's/ && / ; and /g')
-    echo $tmp | sed 's/ || / ; or /g'
-end
-
-function fuck -d 'Correct your previous console command'
-    set -l eval_script (mktemp 2>/dev/null ; or mktemp -t 'thefuck')
-    thefuck $history[1] > $eval_script
-    eval (__thefuck_repl (cat $eval_script))
-    rm $eval_script
-end
+source $OMF_PATH/init.fish
