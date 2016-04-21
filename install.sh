0aff36bb98a84bd6084c26e414b63c2583805a62
hacpai <balckwing@gmail.com>
2016-04-21 17:07:55 +0800

    :memo: Update install

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # @author
 
-# install brew
+# Install brew
 if type brew > /dev/null; then
   echo "Homebrew Exists";
 else
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 fi;
+# Java supported
+brew cask install java7;
+# Go supported
+brew install go;
 # vim supported
 brew install vim --with-lua --HEAD;
 # ag supproted
@@ -22,5 +26,15 @@ ln -s ~/dotfile/.vim/keymap.vim ~/.vim/keymap.vim
 ln -s ~/dotfile/.vim/language.vim ~/.vim/language.vim
 ln -s ~/dotfile/.vim/taglist.vim ~/.vim/taglist.vim
 ln -s ~/dotfile/.vim/style.vim ~/.vim/style.vim
+# YouCompleteMe supported
+if [[ -d ~/.vim/bundle/YouCompleteMe ]]; then
+  echo "YouCompleteMe Exists";
+else
+  git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe;
+  cd ~/.vim/bundle/YouCompleteMe;
+  git submodule update --init --recursive;
+  # for nodejs
+  ./install.py --gocode-completer --clang-completer;
+fi;
 # install vim plugins
 vim +PluginInstall! +qall;
